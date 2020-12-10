a737d5a3066cdb8e12dddc4094ccce62ad16e761
Adri Torres <adriana.amaral@msn.com>
2020-12-10 22:39:02 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,15 +1,7 @@
 def count_strings(array)
-total = 0
-array = [1, 2, 3, 4]
-index = 0
+
  
-while index < array.length do
-  if array[index].even?
-    total += 1 # total is only incremented when the current array element is even
-  end
-  index += 1
-end
-total
+ 
 end
 
 def count_empty_strings(array)
